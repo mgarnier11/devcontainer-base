b6851831347d628a9d38898a9cf6342ab18a405d
Mathieu GARNIER <mgarnier11@gmail.com>
2024-08-01 19:47:11 +0000

    feat: initial commit

---
+++ main.tf
@@ -0,0 +1,101 @@
+# Terraform declarations
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    coder = {
+      source  = "coder/coder"
+      version = ">= 0.17"
+    }
+
+    docker = {
+      source = "kreuzwerker/docker"
+    }
+  }
+}
+
+# Input variables
+variable "agent_id" {
+  type        = string
+  description = "The ID of a Coder agent."
+}
+
+variable "sshd_port" {
+  type        = number
+  description = "The port to the SSH server on."
+  default     = 13338
+}
+
+variable "vscode_web_port" {
+  type        = number
+  description = "The port to run VS Code Web on."
+  default     = 13339
+}
+
+variable "username" {
+  type        = string
+  description = "The username of the workspace user."
+}
+
+variable "setup_script" {
+  type        = string
+  description = "A script to run on workspace setup."
+}
+
+variable "ssh_private_key" {
+  type        = string
+  description = "The SSH private key to setup for the workspace user."
+}
+
+variable "vscode_extensions" {
+  type        = list(string)
+  description = "VS Code extensions to install."
+}
+
+variable "coder_init_script" {
+  type        = string
+  description = "The Coder init script."
+  
+}
+
+# Output variables
+
+output "docker_image_name" {
+  value = "${docker_image.main.name}"
+  
+}
+
+
+
+resource "coder_script" "install" {
+  agent_id = var.agent_id
+  display_name = "Install requirements"
+  script   = templatefile("${path.module}/scripts/install.sh", {})
+  run_on_start = true
+  start_blocks_login = true
+}
+
+
+resource "coder_script" "install-vscode" {
+  agent_id = var.agent_id
+  display_name = "Install requirements"
+  script   = templatefile("${path.module}/scripts/install-vscode-web.sh", {})
+  run_on_start = true
+  start_blocks_login = true
+}
+
+resource "docker_image" "main" {
+  name = "coder-${data.coder_workspace.me.id}"
+  build {
+    context = "./docker"
+    no_cache = true
+    build_args = {
+      USER = var.username
+      SSHD_PORT = var.sshd_port
+      CODER_INIT_SCRIPT = var.coder_init_script
+    }
+  }
+  triggers = {
+    dir_sha1 = sha1(join("", [for f in fileset(path.module, "docker/**") : filesha1(f)]))
+  }
+}
---
+++ scripts/install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+cat <<EOF > ~/.ssh/id_rsa
+-----BEGIN OPENSSH PRIVATE KEY-----
+$SSH_PRIVATE_KEY
+-----END OPENSSH PRIVATE KEY-----
+EOF
+chmod 600 ~/.ssh/id_rsa
+sudo service ssh start
+
+# Zsh Installation
+sh -c "$(wget -O- https://github.com/deluan/zsh-in-docker/releases/download/v1.2.0/zsh-in-docker.sh)"
+printf "ZSH Installed"
+
+# Task Installation
+sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b .local && \
+  echo "export PATH=\$PATH:~/.local" >> ~/.zshrc
+printf "Task Installed"
+
+# Coder Installation
+curl -L https://coder.com/install.sh | sh
+printf "Coder Installed"
+
+# Go Installation
+curl -o go.tar.gz https://dl.google.com/go/go1.22.4.linux-amd64.tar.gz && \
+  sudo tar -C /usr/local -xzf go.tar.gz && \
+  rm go.tar.gz && \
+  echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.zshrc
+printf "Go Installed"
+
+# NVM Installation
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+printf "NVM Installed"
+
+sudo chsh -s /usr/bin/zsh $USER
+
+
+/setup/setup-env.sh
