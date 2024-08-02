7d3c44f043ae41b5061b8410517847eaa90fe60f
Mathieu GARNIER <mgarnier11@gmail.com>
2024-08-02 19:00:57 +0000

    fix: testing soem things

--- docker/scripts/additional-entrypoint.sh
+++ docker/scripts/additional-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-sudo socat UNIX-LISTEN:/var/run/docker.sock,fork,mode=660,user=${USER} UNIX-CONNECT:/var/run/docker-host.sock &
+# sudo socat UNIX-LISTEN:/var/run/docker.sock,fork,mode=660,user=${USER} UNIX-CONNECT:/var/run/docker-host.sock &
