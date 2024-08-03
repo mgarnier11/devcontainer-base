a2d7257d7bd32b4d56829da7ffca12f054e52cfa
Mathieu GARNIER <mgarnier11@gmail.com>
2024-08-03 11:26:18 +0000

    feat: added worksapce file base64 param

--- main.tf
+++ main.tf
@@ -58,7 +58,13 @@ variable "setup_env_script" {
 variable "workspace_file" {
   type        = string
   description = "The vscode workspace file to use."
-  default = "{}"
+  default = ""
+}
+
+variable "workspace_file_base64" {
+  type        = string
+  description = "The base64 encoded vscode workspace file to use."
+  default = ""
 }
 
 variable "vscode_extensions" {
@@ -195,7 +201,7 @@ resource "docker_image" "main" {
       SSHD_PORT = var.sshd_port
       CODER_INIT_SCRIPT = replace(coder_agent.main.init_script, "/localhost|127\\.0\\.0\\.1/", "host.docker.internal")
       SETUP_ENV_SCRIPT = var.setup_env_script
-      WORKSPACE_FILE = var.workspace_file
+      WORKSPACE_FILE = var.workspace_file != "" ? var.workspace_file : base64decode(var.workspace_file_base64)
     }
   }
   triggers = {
